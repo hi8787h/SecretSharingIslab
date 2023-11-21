f96fe5a4a62c1e151ac2988c229a580ec8db0368
wyh329 <wyuhan329@gmail.com>
2023-11-21 14:00:15 +0800

    Update

--- experiment/experiment-2/IoT-Client/codes/client1/LeakageResilientSecretSharing.py
+++ experiment/experiment-2/IoT-Client/codes/client1/LeakageResilientSecretSharing.py
@@ -11,24 +11,44 @@ class LeakageResilientSecretSharing(ShamirSecretSharingBytesStreamer):
 
         def __init__(self):
                 super().__init__(self)
-                #self.s = 
+                self.bin_len = 128
+                self.n = 3
+                self.w = []
+                self.Ext = [] 
 
         #LRshare
-        def choose_para(self):
-                bin_len = 128
-                n = 3
-                w = [] 
-                
-                #parameters s, r, w
-                s = random.choices("01",k=bin_len)
-                r = random.choices("01",k=bin_len)
-                for i in range(n):
-                        w.append(random.choices("01",k=bin_len))
-        
-        def get_Sh(self):
-                lrss = LeakageResilientSecretSharing()
-                lrss.genarate_shares()
 
+        #set parameters
+        def set_s(self):
+                s = random.choices("01",k=self.bin_len)
+    
+                #return a binary list
+                return s
+
+        def set_r(self):
+                r = random.choices("01",k=self.bin_len)
+
+                #return a binary list
+                return r
+
+        def set_w(self):
+                binW = random.choices("01",k=self.bin_len)
+    
+                #return a binary list
+                return binW
+        
+        #compute inner product
+        def xor(self, str1, str2):
+                xor_bin = []
+
+                for i in range(self.bin_len):
+                        int1 = int(str1[i],2)
+                        int2 = int(str2[i],2)
+                        xor_int = int1^int2
+                        xor_bin.append(str(xor_int))
+
+                #return a binary list
+                return xor_bin
 
         #LRrecover
 
