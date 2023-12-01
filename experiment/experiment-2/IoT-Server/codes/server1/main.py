4a0807943786b51ebe9027796c5b6aeacee14750
wyh329 <wyuhan329@gmail.com>
2023-12-01 18:00:07 +0800

    Modify

--- experiment/experiment-2/IoT-Server/codes/server1/main.py
+++ experiment/experiment-2/IoT-Server/codes/server1/main.py
@@ -14,22 +14,26 @@ if __name__ == "__main__":
     while True:
         data_list = []
         
-        data1 = SocketConnection.receive_data(HOST,PORT).decode('utf-8')
-        # test
-        print(data1)
-        part1 = json.loads(data1)
+        try:
+            data1 = SocketConnection.receive_data(HOST,PORT).decode('utf-8')
+            part1 = json.loads(data1)
+            data_list.append(part1)
+        except Exception:
+            pass
 
-        data2 = SocketConnection.receive_data(HOST,PORT).decode('utf-8')
-        # test
-        print(data2)
-        part2 = json.loads(data2)
-        
-        data3 = SocketConnection.receive_data(HOST,PORT).decode('utf-8')
-        # test
-        print(data3)
-        part3 = json.loads(data3)
+        try:
+            data2 = SocketConnection.receive_data(HOST,PORT).decode('utf-8')
+            part2 = json.loads(data2)
+            data_list.append(part2)
+        except Exception:
+            pass
 
-        data_list.append([part1, part2, part3])
+        try:
+            data3 = SocketConnection.receive_data(HOST,PORT).decode('utf-8')
+            part3 = json.loads(data3)
+            data_list.append(part3)
+        except Exception:
+            pass
         
         # Check combined data
         print(data_list)
@@ -38,10 +42,7 @@ if __name__ == "__main__":
         lrss = LeakageResilientSecretSharing()
 
         start_decryption_time = datetime.datetime.now()
-
-        #recover_data = sssbs.combine_shares(data)
         recovered_secret = lrss.leakage_resilient_recovery(data_list)
-
         end_decryption_time =  datetime.datetime.now()
 
         print("[Server] Data SHA256: ",end =" ")
