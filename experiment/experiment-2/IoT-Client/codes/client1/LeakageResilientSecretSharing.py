ff286bf8a42831824870ffd61a2902cd4330133b
wyh329 <wyuhan329@gmail.com>
2023-12-01 14:49:33 +0800

    Modify

--- experiment/experiment-2/IoT-Client/codes/client1/LeakageResilientSecretSharing.py
+++ experiment/experiment-2/IoT-Client/codes/client1/LeakageResilientSecretSharing.py
@@ -89,14 +89,14 @@ class LeakageResilientSecretSharing(ShamirSecretSharingBytesStreamer):
                 S2 = self.S_list[len(self.S_list)//3: 2*len(self.S_list)//3]
                 S3 = self.S_list[2*len(self.S_list)//3: ]
 
-                S1_bytes = json.dumps(S1).encode('utf-8')
-                S2_bytes = json.dumps(S2).encode('utf-8')
-                S3_bytes = json.dumps(S3).encode('utf-8')
-                S_bytes = [S1_bytes, S2_bytes, S3_bytes]
+                #S1 = json.dumps(S1).encode('utf-8')
+                #S2 = json.dumps(S2).encode('utf-8')
+                #S3 = json.dumps(S3).encode('utf-8')
+                S_bytes = [S1, S2, S3]
                 # test
-                print(S1_bytes)
-                print(S2_bytes)
-                print(S3_bytes)
+                print(S1)
+                print(S2)
+                print(S3)
                 #print(len(S_bytes))
                 #print(S_bytes[0])
 
