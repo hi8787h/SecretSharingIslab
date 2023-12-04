969bb8702749662073a2d676793b6ad4a34c38db
tw3673747@gmail.com <tw3673747@gmail.com>
2023-12-04 13:48:22 +0800

    124

--- experiment/experiment-2/IoT-Server/codes/server1/main.py
+++ experiment/experiment-2/IoT-Server/codes/server1/main.py
@@ -6,7 +6,7 @@ import time
 from SocketConnection import SocketConnection
 from HashFunction import HashFunction
 from LeakageResilientSecretSharingReceiver import LeakageResilientSecretSharingReceiver
-
+#change
 HOST = "0.0.0.0"
 PORT = 80
 
