10fb257e49c77cef47a6a88b10eb05c9a0cbe9a5
wyh329 <wyuhan329@gmail.com>
2023-12-15 12:45:03 +0800

    Modify

--- experiment/experiment-2/IoT-Server/codes/server1/LeakageResilientSecretSharing.py
+++ experiment/experiment-2/IoT-Server/codes/server1/LeakageResilientSecretSharing.py
@@ -235,7 +235,7 @@ class LeakageResilientSecretSharing():
 
                 recovered_result = bytes()
                 for i in range(self.k):
-                        sh_pri_rec = self.xor(sh_pri_xor_r_reclist, r_rec)
+                        sh_pri_rec = self.xor(sh_pri_xor_r_reclist[i], r_rec)
                         print('sh_pri_rec:', sh_pri_rec)
                         Ext_rec: bytes = self.get_inner_product(w_reclist[i], s_rec)
                         print('Ext_rec:', Ext_rec)
