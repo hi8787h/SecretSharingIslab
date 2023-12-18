47a240ad750945f1a7b9e56062a01bb51ac76482
wyh329 <wyuhan329@gmail.com>
2023-12-18 21:16:16 +0800

    Modify

--- experiment/experiment-2/IoT-Server/codes/server1/main.py
+++ experiment/experiment-2/IoT-Server/codes/server1/main.py
@@ -10,35 +10,27 @@ PORT = 80
 
 if __name__ == "__main__":
     while True:
-        data_list = []
         received_data_count = 0
-        try:
-            data1 = SocketConnection.receive_data(HOST,PORT).decode('utf-8')
-            part1 = json.loads(data1)
-            print("data 1 received.")
-            data_list += part1
-            received_data_count += 1
-        except Exception:
-            pass
+        data_list = []
+        
+        data1 = SocketConnection.receive_data(HOST,PORT).decode('utf-8')
+        part1 = json.loads(data1)
+        print("data 1 received.")
+        data_list += part1
+        received_data_count += 1
 
-        try:
-            data2 = SocketConnection.receive_data(HOST,PORT).decode('utf-8')
-            part2 = json.loads(data2)
-            print("data 2 received.")
-            data_list += part2
-            received_data_count += 1
-        except Exception:
-            pass
+        data2 = SocketConnection.receive_data(HOST,PORT).decode('utf-8')
+        part2 = json.loads(data2)
+        print("data 2 received.")
+        data_list += part2
+        received_data_count += 1
 
-        try:
-            data3 = SocketConnection.receive_data(HOST,PORT).decode('utf-8')
-            part3 = json.loads(data3)
-            print("data 3 received.")
-            if received_data_count < 2:
-                data_list += part3
-                received_data_count += 1
-        except Exception:
-            pass
+        data3 = SocketConnection.receive_data(HOST,PORT).decode('utf-8')
+        part3 = json.loads(data3)
+        print("data 3 received.")
+        if received_data_count < 2:
+            data_list += part3
+            received_data_count += 1
 
          # If not received enough data to recover secret 
         if received_data_count < 2 :
