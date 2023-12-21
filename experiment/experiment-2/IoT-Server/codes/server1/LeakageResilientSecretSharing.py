3abfa7c9cdaee814930bdfbb8ae5a8c9a6db184a
wyh329 <wyuhan329@gmail.com>
2023-12-21 16:39:41 +0800

    Modify

--- experiment/experiment-2/IoT-Server/codes/server1/LeakageResilientSecretSharing.py
+++ experiment/experiment-2/IoT-Server/codes/server1/LeakageResilientSecretSharing.py
@@ -112,8 +112,7 @@ class LeakageResilientSecretSharing():
                         sr_shares = Shamir.split(self.k, self.n, sr_chunk) 
                         for share in sr_shares:
                                 share_dict = dict()
-                                share_index = share[0] 
-                                print('sr share size:', share[1])
+                                share_index = share[0]
                                 share_data = base64.b64encode(share[1]).decode('utf-8')
                                 share_dict = {
                                 "ChunkID": chunk_id,
@@ -133,8 +132,6 @@ class LeakageResilientSecretSharing():
                         # set parameters
                         shared_s = self.set_s()
                         shared_r = self.set_r()
-                        print(f'Shared s {chunk_id} size:', len(shared_s))
-                        print(f'Shared r {chunk_id} size:', len(shared_r))
                         shared_sr = shared_s + shared_r
                         shared_sr_list = self.generate_sr_shares(shared_sr)
                         # turn sr_shares into bytes
@@ -150,7 +147,6 @@ class LeakageResilientSecretSharing():
                         shared_w_list = []
                         for i in range(self.n):
                                 shared_w = self.set_w()
-                                print(f'Shared w[{chunk_id},{i+1}] size:', len(shared_w))
                                 shared_w_list.append(shared_w)
 
                         shared_Ext_list = []
@@ -170,7 +166,6 @@ class LeakageResilientSecretSharing():
                                 # new share form: (wi, sh' XOR r, Si)
                                 share_data_pri = self.xor(share_data, shared_Ext_list[index])
                                 share_data_pri_X_r = self.xor(share_data_pri, shared_r)
-                                print(f'Shared Sh\' XOR r[{chunk_id},{index+1}] size:', len(share_data_pri_X_r))
                                 new_share_bytes = self.get_new_shares(shared_w_list[index], share_data_pri_X_r, shared_sr_bytes[index])
                                 new_share_data = base64.b64encode(new_share_bytes).decode('utf-8')
                                 index += 1
