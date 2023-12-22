2f3c3dae91f94aa9151066932b3c51100a1fb700
wyh329 <wyuhan329@gmail.com>
2023-12-22 19:29:36 +0800

    Change para

--- experiment/experiment-2/IoT-Server/codes/server1/LeakageResilientSecretSharing.py
+++ experiment/experiment-2/IoT-Server/codes/server1/LeakageResilientSecretSharing.py
@@ -15,7 +15,7 @@ class LeakageResilientSecretSharing():
         def __init__(self):
                 self.bin_len = 128
                 self.modulus = 2 ** self.bin_len
-                self.eta = 9
+                self.eta = 19
                 self.k = 2
                 self.n = 3
                 # For encrypt
