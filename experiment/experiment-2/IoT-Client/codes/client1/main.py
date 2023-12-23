2fb727191427197b70f39e5a9eafc0b0b051a08c
wyh329 <wyuhan329@gmail.com>
2023-12-23 13:23:27 +0800

    Modify

--- experiment/experiment-2/IoT-Client/codes/client1/main.py
+++ experiment/experiment-2/IoT-Client/codes/client1/main.py
@@ -11,44 +11,56 @@ import psutil
 
 if __name__ == "__main__":
     
-    IoT_Info = dict()
-    # Temperature
-    cpu = CPUTemperature()    
-    IoT_Info['Temperature'] = cpu.temperature
-    # CPU & RAM usage
-    IoT_Info['CPU_usage'] = psutil.cpu_percent()
-    IoT_Info['RAM_usage'] = psutil.virtual_memory().percent
+    total_construction_time = 0.0
 
-    Secret = json.dumps(IoT_Info).encode('utf-8')
-    print('Secret :', Secret)
-    print("[Client] Sending data size:", len(Secret), "bytes")
+    for i in range(100):
+        IoT_Info = dict()
+        # Temperature
+        cpu = CPUTemperature()    
+        IoT_Info['Temperature'] = cpu.temperature
+        # CPU & RAM usage
+        IoT_Info['CPU_usage'] = psutil.cpu_percent()
+        IoT_Info['RAM_usage'] = psutil.virtual_memory().percent
 
-    # Hash
-    print("[Client] Data SHA256: ", end =" ")
-    HashFunction.print_sha256(Secret)
+        Secret = json.dumps(IoT_Info).encode('utf-8')
+        print('Secret :', Secret)
+        print("[Client] Sending data size:", len(Secret), "bytes")
 
-    # Secret sharing begins
-    lrss = LeakageResilientSecretSharing()
+        # Hash
+        print("[Client] Data SHA256: ", end =" ")
+        HashFunction.print_sha256(Secret)
 
-    start_time =  datetime.datetime.now() 
-    # Use leakage resilient algorithm on secret
-    lrss_share_list = lrss.genarate_lrShares(Secret)
-    encrypt_end_time =  datetime.datetime.now()
-    print("[Client] Shares construction cost： ", (encrypt_end_time - start_time).total_seconds() ,"sec")
+        # Secret sharing begins
+        lrss = LeakageResilientSecretSharing()
 
-    # Classify the order of shares, send by 3 paths
-    part_1 = lrss.classify_shares(lrss_share_list, 1)
-    part_2 = lrss.classify_shares(lrss_share_list, 2)
-    part_3 = lrss.classify_shares(lrss_share_list, 3)
+        start_time =  datetime.datetime.now() 
+        # Use leakage resilient algorithm on secret
+        lrss_share_list = lrss.genarate_lrShares(Secret)
+        encrypt_end_time =  datetime.datetime.now()
+
+        construction_time = (encrypt_end_time - start_time).total_seconds()
+        total_construction_time += construction_time
+        print("[Client] Shares construction cost： ", construction_time, "sec")
+
+        # Classify the order of shares, send by 3 paths
+        part_1 = lrss.classify_shares(lrss_share_list, 1)
+        part_2 = lrss.classify_shares(lrss_share_list, 2)
+        part_3 = lrss.classify_shares(lrss_share_list, 3)
+        
+        data_1 = json.dumps(part_1).encode('utf-8')
+        data_2 = json.dumps(part_2).encode('utf-8')
+        data_3 = json.dumps(part_3).encode('utf-8')
+
+        print("[Client] Sending data to servers...")
+        pause_time = 0.5
+        SocketConnection.send_data("10.18.173.78", 10001, data_1)
+        time.sleep(pause_time)
+        SocketConnection.send_data("10.18.173.78", 10002, data_2)
+        time.sleep(pause_time)
+        SocketConnection.send_data("10.18.173.78", 10003, data_3)
+
+        time.sleep(1)
+        
+    average_time = round(total_construction_time/100, 4)
+    print('Average share constructing time:', average_time)
     
-    data_1 = json.dumps(part_1).encode('utf-8')
-    data_2 = json.dumps(part_2).encode('utf-8')
-    data_3 = json.dumps(part_3).encode('utf-8')
-
-    print("[Client] Sending data to servers...")
-    pause_time = 0.5
-    SocketConnection.send_data("10.18.173.78", 10001, data_1)
-    time.sleep(pause_time)
-    SocketConnection.send_data("10.18.173.78", 10002, data_2)
-    time.sleep(pause_time)
-    SocketConnection.send_data("10.18.173.78", 10003, data_3)
