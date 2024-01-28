ba6bfd23b6096a20dba9736397ef933978a0b53d
wyh329 <wyuhan329@gmail.com>
2024-01-28 19:42:59 +0800

    0128 Modify

--- experiment/experiment-2/IoT-Server/codes/server1/LeakageResilientSecretSharing.py
+++ experiment/experiment-2/IoT-Server/codes/server1/LeakageResilientSecretSharing.py
@@ -50,7 +50,7 @@ class LeakageResilientSecretSharing():
                         int_2 = int.from_bytes(s_element, byteorder='big')
                         inner_product += int_1 * int_2
                 inner_mod = inner_product % self.modulus
-                inner_bin = bin(inner_mod)[2: ].zfill(16)
+                inner_bin = bin(inner_mod)[2: ].zfill(128)
                 inner_byte = bytes(inner_bin, 'utf-8')
                 return inner_byte
         
@@ -253,6 +253,7 @@ class LeakageResilientSecretSharing():
                         if data['ChunkID'] not in share_id_list:
                                 share_id_list.append(data['ChunkID'])
                                 self.share_chunk_dict[data['ChunkID']] = []
+                        print(recovered_datalist[share_id])
                         share_data_bytes = base64.b64decode(recovered_datalist[share_id])
                         self.share_chunk_dict[chunk_id].append((shareIndex, share_data_bytes))
                         
