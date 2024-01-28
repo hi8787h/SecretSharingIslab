d4c50d6e67fbbb703db529d1b733ae9d794b7870
wyh329 <wyuhan329@gmail.com>
2024-01-29 00:39:12 +0800

    Evaluate

--- experiment/experiment-2/IoT-Client/codes/client1/LeakageResilientSecretSharing.py
+++ experiment/experiment-2/IoT-Client/codes/client1/LeakageResilientSecretSharing.py
@@ -15,7 +15,7 @@ class LeakageResilientSecretSharing():
         def __init__(self):
                 self.bin_len = 16
                 self.modulus = 2 ** self.bin_len
-                self.eta = 3
+                self.eta = 4
                 self.k = 2
                 self.n = 3
                 # For encrypt
